441b041613becdd85fa870e33f5db05e4ef2e9ea
unknown <cirejuniorba@gmail.com>
2023-01-05 10:57:02 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import {Flex, Box, Text, Button} from '@chakra-ui/react'
+
+const Banner = ({purpose, title1, title2, desc1, desc2, linkName, buttonText, imageUrl
+}) => (
+	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10" >
+		<Image src={imageUrl} width={500} height={300} alt='Banner' />
+		<Box p={5}>
+			<Text color="gray.500" fontSize="sm" fontWeight="medium" > {purpose} </Text>
+			<Text fontSize="3xl" fontWeight="bold" > {title1} <br /> {title2} </Text>
+			<Text color="gray.700" fontSize="lg" paddingY={3} > {desc1} <br /> {desc2} </Text>
+			<Button fontSize="xl">
+				<Link href={linkName}> {buttonText} </Link>
+			</Button>
+		</Box>
+	</Flex>
+)
+
+export default function Home() {
+  return (
+	<Box>
+		<Banner 
+		purpose="RENT A HOME"
+		title1="Rental Homes for "
+		title2="Everyone"
+		desc1="Explore Apartments, Villas, Homes"
+		desc2="and more"
+		imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+		linkName="/search?purpose=for-rent"
+		buttonText="Explore Renting "
+		/>
+
+		<Flex flexWrap="wrap">
+
+		</Flex>
+
+		<Banner 
+		purpose="BUY A HOME"
+		title1="Find, Buy & Own Your"
+		title2="Dream Home"
+		desc1="Explore Apartments, Villas, Homes"
+		desc2="and more"
+		imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+		linkName="/search?purpose=for-sale"
+		buttonText="Explore Buying" 
+		/>
+	</Box>
+  )
+}
